dbe4675b98fa5c2edc85dd8b271ba3058d174bfe
feradoxin <sam.wong@protonmail.ch>
2021-05-11 10:25:36 +0800

    Change to 256-bit entropy and 24-word mnemonic.

--- keyDerive.js
+++ keyDerive.js
@@ -5,7 +5,7 @@ const wif = require('wif');
 
 /* Generate 128-bit entropy, create mnemonic from entropy then convert to master seed, create root private key from master seed.
    Mnemonic generation to be replaced with user input for pairing to Trezor/Ledger in NFT Frame App. The code must be offline to prevent leak of keys.*/
-const mnemonic = "punch plug gadget noble brush maximum awful bring resource denial flash brand ketchup artefact divorce review taste charge echo asset flee crop click load" //bip39.generateMnemonic();
+const mnemonic = "punch plug gadget noble brush maximum awful bring resource denial flash brand ketchup artefact divorce review taste charge echo asset flee crop click load" //bip39.generateMnemonic("256");
 const seed = bip39.mnemonicToSeedSync(mnemonic);
 const isValid = bip39.validateMnemonic(mnemonic);
 const m = hdKey.fromMasterSeed(seed);
@@ -21,12 +21,13 @@ const ethXpriv = m.derivePath(hdPath).privateExtendedKey();
 const ethPubHDKey = hdKey.fromExtendedKey(ethXpub);
 const ethPrivHDKey = hdKey.fromExtendedKey(ethXpriv);
 
-// Generate Wallet 0
-const ethWalletAddress = ethPubHDKey.derivePath("m/0").getWallet().getAddressString();
-const ethWalletPub = ethPubHDKey.derivePath("m/0").getWallet().getPublicKeyString();
-const ethWalletPriv = ethPrivHDKey.derivePath("m/0").getWallet().getPrivateKeyString();
+// Generate Wallet. Replace ethAddrIndex to derive downstream addresses.
+const ethAddrIndex = "0"
+const ethWalletAddress = ethPubHDKey.derivePath("m/" + ethAddrIndex).getWallet().getAddressString();
+const ethWalletPub = ethPubHDKey.derivePath("m/" + ethAddrIndex).getWallet().getPublicKeyString();
+const ethWalletPriv = ethPrivHDKey.derivePath("m/" + ethAddrIndex).getWallet().getPrivateKeyString();
 
-// Logs to check where shit goes wrong...or right! Well hopefully the latter.
+// Logs. More logs. All the logs.
 console.log("------");
 console.log("BIP39 Mnemonic: " + mnemonic);
 console.log("Mnemonic is Valid: " + isValid);
